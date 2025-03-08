4ed5479e6e2840e685050862e79fa4650c1de38c
gapinsky <gapinski02@gmail.com>
2025-03-08 22:17:01 +0100

    chakra ui & tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react(), tsconfigPaths()],
 });
